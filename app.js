aa8af20c8802ffb985da69f6f2884723fa2f8f8f
birciceke <birciceke@gmail.com>
2025-12-24 17:14:16 +0300

    Session fix update.

--- app.js
+++ app.js
@@ -45,7 +45,11 @@ const sessionConfig = {
   secret: process.env.SECRET_KEY,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 },
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 1000 * 60 * 60 * 24,
+  },
 };
 
 connection();
